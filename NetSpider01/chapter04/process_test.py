48a7980f4984f98effc47a13c2f71a6cff9206ed
motein <motein@outlook.com>
2018-06-16 19:05:48 +0800

    Update

--- NetSpider01/chapter04/process_test.py
+++ NetSpider01/chapter04/process_test.py
@@ -3,3 +3,17 @@ Created on Jun 15, 2018
 
 @author: xiongan2
 '''
+import sys
+from chapter04.process_crawler import process_crawler
+from chapter03.mongo_cache import MongoCache
+from chapter04.alexa_cb import AlexaCallback
+
+
+def main(max_threads):
+    scrape_callback = AlexaCallback()
+    cache = MongoCache()
+    cache.clear()
+    process_crawler(scrape_callback.seed_url, scrape_callback=scrape_callback, cache=cache, max_threads=max_threads, timeout=10)
+    
+max_threads = int(4)
+main(max_threads)
